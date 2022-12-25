4597e52fbedd509305dd2f7411672fbfcfc2c4fe
SamsonMokaya <mokayasamson950@gmail.com>
2022-12-24 21:55:52 -0800

    Hosting server side on render

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     //fetch data from the server
 
-    const response = await fetch('https://chatgpt-codex.onrender.com', {
+    const response = await fetch('https://chatgpt-codex-ai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
